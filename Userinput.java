63901024f9429cd443d26678661ad7a550e5a180
sravan331 <107671162+sravan331@users.noreply.github.com>
2022-06-24 11:19:40 +0530

    Update Userinput.java

--- Userinput.java
+++ Userinput.java
@@ -1,9 +1,19 @@
+// @author sravan kumar
+
+// userinput programe
+
 import java.util.Scanner;
+
 class Userinput
 {
-public static void main(String args[])
+
+  public static void main(String args[])
 {
-Scanner bec = new Scanner(System.in);
-System.out.println(bec.nextLine());
+// we have to write any name or sentence
+    
+    Scanner bec = new Scanner(System.in);
+// displaying the name what we have written
+    System.out.println(bec.nextLine());
 }
+  
 }
